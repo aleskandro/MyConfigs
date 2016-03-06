b01893e6054d02b21c1cfada13b0e4c83c5d1921
aleskandro <aleskandro@lucylaika.ovh>
2016-03-06 13:18:09 +0100

    Various configurations file and script

---
+++ home/aleskandro/.zshrc
@@ -0,0 +1,12 @@
+# azzoppati {{{
+[ -s "${ZDOTDIR:-$HOME}/.zoppo/zoppo.zsh" ] && source "${ZDOTDIR:-$HOME}/.zoppo/zoppo.zsh"
+# }}}
+#
+PATH=/usr/local/bin:/usr/bin:/bin:/opt/bin:/usr/x86_64-pc-linux-gnu/gcc-bin/4.8.5:/usr/x86_64-pc-linux-gnu/avr/gcc-bin/4.8.5:/home/aleskandro/Android/Sdk/platform-tools:/home/aleskandro/Android/Sdk/tools:/home/aleskandro/Android/android-ndk-r10e
+NDKROOT=/home/aleskandro/Android/android-ndk-r10e
+#eval `keychain --eval ~/.ssh/id_rsa`
+# vim: ft=zsh sts=2 ts=2 sw=2 et fdm=marker fmr={{{,}}
+#
+#eval `keychain --eval id_rsa`
+SSH_AUTH_SOCK=/home/aleskandro/.ssh/agent.sock; export SSH_AUTH_SOCK;
+
---
+++ usr/local/bin/alarm.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+while : 
+do
+	echo "Press CTRL+C to stop"
+	date
+        beep -f 400 -l 20 -n -f 500 -l 20 -n -f 600 -l 20 -n -f 700 -l 20 -n -f 800 -l 20 -n -f 900 -l 20 -n -f 1000 -l 20 -n -f 1100 -l 20 -n -f 1200 -l 20 -n -f 1300 -l 20 -n -f 1400 -l 20 -n -f 1500 -l 20 -n -f 1600 -l 20 -n -f 400 -l 20 -n -f 500 -l 20 -n -f 600 -l 20 -n -f 700 -l 20 -n -f 800 -l 20 -n -f 900 -l 20 -n -f 1000 -l 20 -n -f 1100 -l 20 -n -f 1200 -l 20 -n -f 1300 -l 20 -n -f 1400 -l 20 -n -f 1500 -l 20 -n -f 1600 -l 20 -n  -f 400 -l 20 -n -f 500 -l 20 -n -f 600 -l 20 -n -f 700 -l 20 -n -f 800 -l 20 -n -f 900 -l 20 -n -f 1000 -l 20 -n -f 1100 -l 20 -n -f 1200 -l 20 -n -f 1300 -l 20 -n -f 1400 -l 20 -n -f 1500 -l 20 -n -f 1600 -l 20 -n  -f 400 -l 20 -n -f 500 -l 20 -n -f 600 -l 20 -n -f 700 -l 20 -n -f 800 -l 20 -n -f 900 -l 20 -n -f 1000 -l 20 -n -f 1100 -l 20 -n -f 1200 -l 20 -n -f 1300 -l 20 -n -f 1400 -l 20 -n -f 1500 -l 20 -n -f 1600 -l 20 &
+
+	sleep 1
+	
+	clear
+done
