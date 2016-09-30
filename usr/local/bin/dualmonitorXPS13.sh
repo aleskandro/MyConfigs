cb86d72991ef22b4721b1995293c0a4743fb2eb2
Alessandro Di Stefano <aleskandro@lucylaika.ovh>
2016-09-30 20:46:52 +0200

    Update dualmonitorXPS13.sh

--- usr/local/bin/dualmonitorXPS13.sh
+++ usr/local/bin/dualmonitorXPS13.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# A basic script to get working two monitors and touchscreen on hidpi xps13 2016 with Xorg. It scales the second monitor and restart plasma. The second monitor appears with a lof of blurry text but with scale to 1.6 and DPI to 158 I got a good compromise to work with two screens. see on MyConfigs/usr/local/bin/dualmonitorXPS13.sh
 # Monitor settings
 EDP1W=3200
 EDP1H=1800
